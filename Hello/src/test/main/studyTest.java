543fc92e28dcbc5ef6a80a4f7970ee1dc48e69ee
hyerim <yhrim0729@naver.com>
2021-04-30 18:06:42 +0900

    "3차 테스트"

--- Hello/src/test/main/studyTest.java
+++ Hello/src/test/main/studyTest.java
@@ -4,5 +4,6 @@ public class studyTest {
 	public static void main(String[] args) {
 		System.out.println("1차 테스트입니다.");
 		System.out.println("2차 테스트입니다.");
+		System.out.println("3차 테스트입니다.");
 	}
 }
